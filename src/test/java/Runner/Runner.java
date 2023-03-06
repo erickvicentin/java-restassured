dd80144da04c3e87328ee2992cb5eb36c50e53e6
Erick Emanuel Vicentin <erickvicentin16@gmail.com>
2023-03-06 03:01:48 -0300

    Added Cucumber options

---
+++ src/test/java/Runner/Runner.java
@@ -0,0 +1,15 @@
+package Runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "Steps"
+)
+
+public class Runner {
+
+}
