b92cd343ad25c9e4f6437cfd8fe080580e9834e5
Erick Emanuel Vicentin <erickvicentin16@gmail.com>
2023-03-06 02:56:56 -0300

    Added new steps

--- src/test/java/Steps/Steps.java
+++ src/test/java/Steps/Steps.java
@@ -1,2 +1,16 @@
-package Steps;public class Steps {
+package Steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class Steps {
+    @Given("I send a GET request to the endpoint")
+    public void sendGetRequest(){
+        System.out.println("Sending GET request...");
+    }
+
+    @Then("I get a list with  10 users")
+    public void getUserList(){
+        System.out.println("Getting user list...");
+    }
 }
