b61fe9764f31065431783ee0230da57b24d02ac1
Erick Emanuel Vicentin <erickvicentin16@gmail.com>
2023-03-07 00:14:35 -0300

    Added steps and assert

--- src/test/java/Steps/Steps.java
+++ src/test/java/Steps/Steps.java
@@ -1,16 +1,31 @@
 package Steps;
 
+import static io.restassured.RestAssured.given;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 
 public class Steps {
-    @Given("I send a GET request to the endpoint")
-    public void sendGetRequest(){
-        System.out.println("Sending GET request...");
-    }
 
-    @Then("I get a list with  10 users")
-    public void getUserList(){
-        System.out.println("Getting user list...");
-    }
+  private static RequestSpecification request;
+  private static Response response;
+  private static ValidatableResponse json;
+
+  @Given("I send a GET request to the endpoint")
+  public void sendGETRequest() {
+
+    request = given().baseUri("https://api.github.com").contentType(ContentType.JSON);
+  }
+
+  @Then("I get a {int} status code")
+  public void expectedStatusCode(int expectedStatusCode) {
+
+    response = request.when().get("/users/erickvicentin/repos");
+
+    json = response.then().statusCode(expectedStatusCode);
+  }
 }
