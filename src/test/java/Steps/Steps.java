5cd446bf2b53188d8d07d0f6b657cd76facf89b3
Erick Emanuel Vicentin <erickvicentin16@gmail.com>
2023-03-09 14:54:04 -0300

    Added new TCs

--- src/test/java/Steps/Steps.java
+++ src/test/java/Steps/Steps.java
@@ -1,6 +1,10 @@
 package Steps;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -15,17 +19,41 @@ public class Steps {
   private static Response response;
   private static ValidatableResponse json;
 
-  @Given("I send a GET request to the endpoint")
-  public void sendGETRequest() {
-
-    request = given().baseUri("https://api.github.com").contentType(ContentType.JSON);
+  @Given("I send a GET request to the {string} URI")
+  public void sendGETRequest(String uri) {
+    request = given().baseUri(uri).contentType(ContentType.JSON);
   }
 
   @Then("I get a {int} status code")
   public void expectedStatusCode(int expectedStatusCode) {
-
     response = request.when().get("/users/erickvicentin/repos");
-
     json = response.then().statusCode(expectedStatusCode);
+  }
+
+  @Then("I validate there are {int} items on the {string} enpoint")
+  public void expectedItems(int items, String path){
+    response = request.when().get(path);
+    
+    List<String> jsonResponse = response.jsonPath().getList("$");
+    int actualSize = jsonResponse.size();
+    assertEquals(items, actualSize);
+  }
+
+  
+  @Then("I validate there is a value: {string} in the response at {string} endpoint")
+  public void validateValue(String expectedName, String path){
+    response = request.when().get(path);
+    
+    List<String> jsonResponse = response.jsonPath().getList("username");
+    assertTrue("No se encontro el valor "+expectedName, jsonResponse.contains(expectedName));
+  }
+
+  @Then("I can validate the nested: {string} on the response at {string} endpoint")
+  public void validateNestedValue(String expectedStreet, String path){
+    response = request.when().get(path);
+    
+    String jsonResponse = response.jsonPath().getString("address.street");
+    
+    assertTrue("La calle "+expectedStreet+" no pertenecee a ningun usuario.", jsonResponse.contains(expectedStreet));
   }
 }
--- src/test/resources/features/APITest.feature
+++ src/test/resources/features/APITest.feature
@@ -2,5 +2,21 @@ Feature: API TESTING
 
   @API
   Scenario: Test GET to an endpoint
-    Given I send a GET request to the endpoint
+    Given I send a GET request to the "https://api.github.com" URI
     Then I get a 200 status code
+
+  @API
+  Scenario: Validate users amount
+    Given I send a GET request to the "https://jsonplaceholder.typicode.com" URI
+    Then I validate there are 10 items on the "/users" enpoint
+
+  @API
+  Scenario: Validate user value is present
+    Given I send a GET request to the "https://jsonplaceholder.typicode.com" URI
+    Then I validate there is a value: "Bilardo" in the response at "/users" endpoint
+
+  @API
+  Scenario: Validate user street is present
+    Given I send a GET request to the "https://jsonplaceholder.typicode.com" URI
+    Then I can validate the nested: "Dayna Park" on the response at "/users" endpoint
+    
